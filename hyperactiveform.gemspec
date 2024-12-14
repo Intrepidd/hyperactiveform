3821c0abf8a7c1a92db7a2408d1b0ec39d2583b0
Adrien Siami <adrien@siami.fr>
2024-12-14 13:17:43 +0100

    Add some yard doc

--- hyperactiveform.gemspec
+++ hyperactiveform.gemspec
@@ -33,4 +33,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "rails", ">= 7.0"
   spec.add_development_dependency "rspec", "~> 3.13"
+  spec.add_development_dependency "yard"
 end
--- lib/hyper_active_form/base.rb
+++ lib/hyper_active_form/base.rb
@@ -4,6 +4,13 @@ require "active_model"
 require "action_controller"
 
 module HyperActiveForm
+  ##
+  # Base class for HyperActiveForm objects
+  #
+  # HyperActiveForm objects are simple ActiveModel objects that encapsulate
+  # form logic and validations. They are designed to be subclassed and
+  # customized to fit the needs of your application.
+  #
   class Base
     include ActiveModel::Model
     include ActiveModel::Attributes
@@ -12,6 +19,11 @@ module HyperActiveForm
 
     define_model_callbacks :assign_form_attributes, :submit
 
+    # Defines to which object the form should delegate the active model methods
+    # This is useful so `form_for`/`form_with` can automatically deduce the url and method to use
+    #
+    # @param klass [Class] the class of the object to proxy
+    # @param object [Object] where to delegate the object to, for example: `:@user`
     def self.proxy_for(klass, object)
       delegate :new_record?, :persisted?, :id, to: object
       singleton_class.delegate :model_name, to: klass
@@ -28,6 +40,12 @@ module HyperActiveForm
       raise NotImplementedError
     end
 
+    # Assigns the attributes of the form from the params
+    # This method is called by the `submit` method, but can also be called
+    # directly if you need to assign the attributes without submitting the form
+    # for example if you want to refresh the form with new data
+    #
+    # @param params [Hash] the params to assign the attributes from
     def assign_form_attributes(params)
       run_callbacks :assign_form_attributes do
         params = ActionController::Parameters.new(params) unless params.is_a?(ActionController::Parameters)
@@ -38,6 +56,11 @@ module HyperActiveForm
       end
     end
 
+    # Submits the form, assigning the attributes from the params,
+    # running validations and calling the `perform` method if the form is valid
+    #
+    # @param params [Hash] the params to assign the attributes from
+    # @return [Boolean] true if the form is valid and the `perform` method returned something truthy
     def submit(params)
       run_callbacks :submit do
         assign_form_attributes(params)
@@ -47,10 +70,17 @@ module HyperActiveForm
       false
     end
 
+    # Same as `submit` but raises a `FormDidNotSubmitError` if the form is not valid
+    #
+    # @param params [Hash] the params to assign the attributes from
+    # @return [Boolean] true if the form is valid and the `perform` method returned something truthy
     def submit!(params)
       submit(params) || raise(HyperActiveForm::FormDidNotSubmitError)
     end
 
+    # Adds the errors from a model to the form
+    #
+    # @param model [ActiveModel::Model] the model to add the errors from
     def add_errors_from(model)
       model.errors.each do |error|
         Array.wrap(error.message).each { |e| errors.add(error.attribute, e) }
